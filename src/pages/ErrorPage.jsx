68b87b76818410017ede3aa1bac48651f24f1a7d
shakiqurrahman <rahmanshakiqur@gmail.com>
2024-08-12 19:04:08 +0600

    errorPage added

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
+import errorImg from "../assets/images/not-found-icon.jpg";
 
 const ErrorPage = () => {
-  return (
-    <>
-      <h1>404, Oops! Page not found!</h1>
-    </>
-  )
-}
+    return (
+        <section className="max-width flex flex-col items-center justify-center min-h-[50vh]">
+            <img className="size-32" src={errorImg} alt="error image" />
+            <h1>404, Oops! Page not found!</h1>
+        </section>
+    );
+};
 
 export default ErrorPage;
-
