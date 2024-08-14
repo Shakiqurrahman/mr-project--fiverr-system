517dc445ca1b8f1a6a2fad62b6904c15cfd20b7a
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-08-15 00:14:52 +0600

    hero completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import bigDealIcon from "../assets/images/Big Deal.svg";
 import heroBanner from "../assets/images/Home Page Banner.jpg";
 import shape from "../assets/images/free_shape.png";
 
 const Hero = () => {
+    const { control, handleSubmit, watch, setValue } = useForm();
     const [selectedItems, setSelectedItems] = useState([]);
+    const [dropdownVisible, setDropdownVisible] = useState({});
 
-    const handleCheckboxChange = (event) => {
-        const { name, checked } = event.target;
+    const watchAllFields = watch();
 
+    const handleCheckboxChange = (name, checked) => {
         if (checked && selectedItems.length < 3) {
             setSelectedItems([...selectedItems, name]);
+            setDropdownVisible((prev) => ({ ...prev, [name]: true }));
         } else if (!checked) {
             setSelectedItems(selectedItems.filter((item) => item !== name));
+            setDropdownVisible((prev) => ({ ...prev, [name]: false }));
+            setValue(`${name}_single_side`, false);
+            setValue(`${name}_double_side`, false);
         }
     };
 
@@ -20,15 +27,19 @@ const Hero = () => {
         return selectedItems.length >= 3 && !selectedItems.includes(name);
     };
 
+    const onSubmit = (data) => {
+        const filteredData = Object.entries(data).filter(
+            ([key, value]) => value === true
+        ); // for showing only those values are true
+        console.log("Filtered Data:", Object.fromEntries(filteredData));
+    };
+
     const checkboxData = [
         { name: "door_hanger", label: "Door Hanger" },
         { name: "rack_card", label: "Rack Card" },
         { name: "billboard", label: "Billboard" },
         { name: "flyer", label: "Flyer" },
-        {
-            name: "Social_Media_Post",
-            label: "Social Media",
-        },
+        { name: "Social_Media_Post", label: "Social Media" },
         { name: "yard_sign", label: "Yard Sign" },
         { name: "postcard", label: "Postcard" },
         { name: "facebook_cover", label: "Facebook Cover" },
@@ -40,10 +51,10 @@ const Hero = () => {
             className="py-20 flex items-center bg-cover bg-center"
             style={{ backgroundImage: `url(${heroBanner})` }}
         >
-            <div className="max-width flex flex-col lg:flex-row gap-20 items-center">
+            <div className="max-width flex flex-col lg:flex-row gap-16 lg:gap-10 items-center">
                 <div className="w-full">
-                    <h2 className="text-center lg:text-left text-2xl sm:text-4xl font-bold uppercase text-primary mb-4">
-                        WE specialize in crea ting advertisement designs.
+                    <h2 className="text-center lg:text-left text-2xl sm:text-[36px] font-bold uppercase text-primary leading-snug mb-4">
+                        WE specialize in creating advertisement designs.
                     </h2>
                     <p className="text-center lg:text-left text-lg sm:text-2xl">
                         You can create any advertising design for your business
@@ -73,28 +84,96 @@ const Hero = () => {
                         together
                     </p>
 
-                    <form className="grid sm:grid-cols-2 md:grid-cols-3 mt-4 gap-2">
+                    <form
+                        onSubmit={handleSubmit(onSubmit)}
+                        className="grid sm:grid-cols-2 lg:grid-cols-3 mt-4 gap-2"
+                    >
                         {checkboxData.map((item) => (
                             <div
-                                className="flex items-center gap-2"
+                                className="flex flex-col gap-2"
                                 key={item.name}
                             >
-                                <input
-                                    className="size-5 accent-[#ed8864]"
-                                    type="checkbox"
-                                    name={item.name}
-                                    id={item.name}
-                                    onChange={handleCheckboxChange}
-                                    disabled={isDisabled(item.name)}
-                                />
-                                <label
-                                    className="text-lg select-none"
-                                    htmlFor={item.name}
-                                >
-                                    {item.label}
-                                </label>
+                                <div className="flex items-center gap-2">
+                                    <Controller
+                                        name={item.name}
+                                        control={control}
+                                        render={({ field }) => (
+                                            <label className="flex items-center gap-2 cursor-pointer">
+                                                <input
+                                                    className="size-4 accent-[#ed8864]"
+                                                    type="checkbox"
+                                                    id={item.name}
+                                                    {...field}
+                                                    onChange={(e) => {
+                                                        field.onChange(e);
+                                                        handleCheckboxChange(
+                                                            item.name,
+                                                            e.target.checked
+                                                        );
+                                                    }}
+                                                    disabled={isDisabled(
+                                                        item.name
+                                                    )}
+                                                />
+                                                <span className="text-[17px] select-none">
+                                                    {item.label}
+                                                </span>
+                                            </label>
+                                        )}
+                                    />
+                                </div>
+                                {dropdownVisible[item.name] && (
+                                    <div
+                                        className={`ml-6 space-y-1 transition-all duration-300 ease-in-out ${
+                                            dropdownVisible[item.name]
+                                                ? "max-h-20 opacity-100"
+                                                : "max-h-0 opacity-0"
+                                        } overflow-hidden`}
+                                    >
+                                        <label className="flex items-center gap-2 cursor-pointer">
+                                            <Controller
+                                                name={`${item.name}_single_side`}
+                                                control={control}
+                                                render={({ field }) => (
+                                                    <input
+                                                        type="checkbox"
+                                                        {...field}
+                                                        className="accent-[#ed8864]"
+                                                    />
+                                                )}
+                                            />
+                                            <span className="select-none text-sm">
+                                                Single Side
+                                            </span>
+                                        </label>
+                                        <label className="flex items-center gap-2 cursor-pointer">
+                                            <Controller
+                                                name={`${item.name}_double_side`}
+                                                control={control}
+                                                render={({ field }) => (
+                                                    <input
+                                                        type="checkbox"
+                                                        {...field}
+                                                        className="accent-[#ed8864]"
+                                                    />
+                                                )}
+                                            />
+                                            <span className="select-none text-sm">
+                                                Double Side
+                                            </span>
+                                        </label>
+                                    </div>
+                                )}
                             </div>
                         ))}
+                        <div className="absolute -bottom-5 right-8">
+                            <button
+                                type="submit"
+                                className="bg-primary uppercase text-white py-2 px-4 font-bold rounded-[30px]"
+                            >
+                                Project Start
+                            </button>
+                        </div>
                     </form>
                 </div>
             </div>
