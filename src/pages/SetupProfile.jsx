a52f2116861f185492cd9431b1a26dbf7d7950a2
Developer Shakil <itsdevelopershakil@gmail.com>
2024-09-03 15:51:25 +0600

    SetupProfile Image Resolution fixed

--- src/pages/SetupProfile.jsx
+++ src/pages/SetupProfile.jsx
@@ -175,7 +175,7 @@ function SetupProfile({ from_profile }) {
       >
         <div className="text-center">
           <div className="mx-auto mb-10 flex h-[220px] w-[220px] items-center justify-center overflow-hidden rounded-full bg-[#DCEEFA]">
-            <label htmlFor="image">
+            <label htmlFor="image" className="h-full w-full">
               {form.image ? (
                 uploading ? (
                   <Box sx={{ display: "flex", alignItems: "center" }}>
@@ -185,7 +185,7 @@ function SetupProfile({ from_profile }) {
                   <img
                     src={form.image}
                     alt="Profile"
-                    className="cursor-pointer object-cover"
+                    className="h-full w-full cursor-pointer object-cover"
                   />
                 )
               ) : (
@@ -193,7 +193,7 @@ function SetupProfile({ from_profile }) {
                 <img
                   src={Avatar}
                   alt="Profile"
-                  className="cursor-pointer object-contain"
+                  className="h-full w-full cursor-pointer object-cover"
                 />
               )}
             </label>
