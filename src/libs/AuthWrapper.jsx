95ae528860b83efa752d1590038a478a515868ce
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-09-20 18:14:22 +0600

    cleaned code

--- src/libs/AuthWrapper.jsx
+++ src/libs/AuthWrapper.jsx
@@ -3,7 +3,6 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useFetchUserDataQuery } from "../Redux/api/apiSlice";
 import { logout, setUser } from "../Redux/features/userSlice";
-import { connectSocket, disconnectSocket } from "./socketService";
 
 const AuthWrapper = ({ children }) => {
   const dispatch = useDispatch();
@@ -18,17 +17,6 @@ const AuthWrapper = ({ children }) => {
       dispatch(logout(error));
     } else if (user) {
       dispatch(setUser({ user: user.data, token: Cookies.get("authToken") }));
-    }
-    if (user && !Cookies.get("authToken")) {
-      connectSocket("http://localhost:3000", {
-        auth: {
-          token: Cookies.get("authToken"),
-        },
-      });
-  
-      return () => {
-        disconnectSocket();
-      };
     }
   }, [user, error, dispatch]);
 
