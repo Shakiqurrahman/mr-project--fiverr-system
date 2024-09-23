eb9bfe3e3557f70e24c082896f88deacb750d6ad
Developer Shakil <itsdevelopershakil@gmail.com>
2024-09-23 21:12:05 +0600

    HomeActivityCards Responsive done.

--- src/components/HomeActivityCards.jsx
+++ src/components/HomeActivityCards.jsx
@@ -1,58 +1,7 @@
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel/slick/slick.css";
-
 function HomeActivityCards() {
-  const settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    arrows: false,
-    draggable: false,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 1,
-          draggable: true,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          draggable: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          draggable: true,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          draggable: true,
-          autoplaySpeed: 2000,
-          infinite: true,
-        },
-      },
-    ],
-  };
   return (
     <div className="max-width mt-10">
       <div className="homeactivitycard grid grid-cols-2 gap-3 sm:grid-cols-3 lg:grid-cols-5">
-        {/* <Slider {...settings}> */}
         <div className="h-full">
           <div className="h-full rounded-xl bg-primary px-3 py-5 text-center text-white">
             <span className="text-2xl font-semibold sm:text-3xl">8+</span>
@@ -96,7 +45,6 @@ function HomeActivityCards() {
             </h3>
           </div>
         </div>
-        {/* </Slider> */}
       </div>
     </div>
   );
