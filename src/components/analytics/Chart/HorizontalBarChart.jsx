d8f232148c2bc040a9acd5409aaf3c2355fd3867
shakiqurrahman <rahmanshakiqur@gmail.com>
2024-09-26 21:25:59 +0600

    horizontal bar added

--- src/components/analytics/Chart/HorizontalBarChart.jsx
+++ src/components/analytics/Chart/HorizontalBarChart.jsx
@@ -7,7 +7,7 @@ const HorizontalBarChart = ({ data, title }) => {
 
   // Define colors for each category using a mapping object
   const colorMapping = {
-    completed: "#1b8cdc", 
+    completed: "#1b8cdc",
     offer: "#808000", // Olive color code
     custom: "#ffa500", // Orange color
     revision: "#f1592a",
@@ -29,12 +29,12 @@ const HorizontalBarChart = ({ data, title }) => {
   const [hoveredIndex, setHoveredIndex] = useState(null);
 
   return (
-    <div className="flex flex-col items-center mb-14">
+    <div className="mb-14 flex flex-col items-center">
       <div className="w-full">
         {/* Custom Legend */}
-        <div className="mb-6 flex flex-col md:flex-row items-center gap-4">
+        <div className="mb-6 flex flex-col items-center gap-4 md:flex-row">
           <h2 className="text-xl font-semibold text-primary">{title}</h2>
-          <div className="flex items-center justify-center flex-1 flex-wrap">
+          <div className="flex flex-1 flex-wrap items-center justify-center">
             {categories.map((label, index) => (
               <div className="mx-2 flex items-center" key={index}>
                 <div
@@ -52,36 +52,75 @@ const HorizontalBarChart = ({ data, title }) => {
 
         {/* Full-width Single Bar */}
         <div className="mb-5 flex w-full items-center">
-          <div className="relative h-6 flex-1 overflow-hidden rounded-[20px] bg-gray-300 ">
-            {percentages.map((percentage, index) => {
-              const leftOffset = percentages
-                .slice(0, index)
-                .reduce((sum, pct) => sum + pct, 0);
-
-              return (
-                <div
-                  key={index}
-                  className="absolute h-full transition-opacity duration-200"
-                  style={{
-                    left: `${leftOffset}%`,
-                    width: `${percentage}%`,
-                    backgroundColor: colorMapping[categories[index]] || "#ccc", // Fallback color
-                    cursor: "pointer",
-                  }}
-                  onMouseEnter={() => setHoveredIndex(index)}
-                  onMouseLeave={() => setHoveredIndex(null)}
-                >
-                  {/* Show percentage on hover */}
-                  <span
-                    className={`flex h-full items-center justify-center text-xs text-white transition-opacity duration-200 ${
-                      hoveredIndex === index ? "opacity-100" : "opacity-0"
-                    }`}
+          <div className="relative   w-full flex-1 ">
+            <div className="bg-white p-1 flex rounded-[20px]">
+              {percentages.map((percentage, index) => {
+                return (
+                  <div
+                    key={index}
+                    className={`flex h-6 transition-opacity duration-200 first:rounded-l-[20px] last:rounded-r-[20px]`}
+                    style={{
+                      // left: `${leftOffset}%`,
+                      width: `${percentage}%`,
+                      backgroundColor:
+                        colorMapping[categories[index]] || "#ccc",
+                      cursor: "pointer",
+                    }}
+                    onMouseEnter={() => setHoveredIndex(index)}
+                    onMouseLeave={() => setHoveredIndex(null)}
                   >
-                    {percentage.toFixed(1)}%
-                  </span>
-                </div>
-              );
-            })}
+                    {/* Show percentage on hover */}
+                    <span
+                      className={`flex w-full h-full items-center justify-center text-sm text-white transition-opacity duration-200 ${
+                        hoveredIndex === index ? "opacity-100" : "opacity-0"
+                      }`}
+                    >
+                      {percentage.toFixed(1)}%
+                    </span>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="absolute top-[-5px] z-[-2] left-1 flex h-[calc(100%_+_10px)] w-[calc(100%_-_8px)]">
+              {percentages.map((percentage, index) => {
+                const leftOffset = percentages
+                  .slice(0, index)
+                  .reduce((sum, pct) => sum + pct, 0);
+                return (
+                  <div
+                    key={index}
+                    className={`asbolute top-0 h-full transition-opacity duration-200 first:rounded-l-[20px] last:rounded-r-[20px]`}
+                    style={{
+                      left: `calc(${leftOffset}% - 5px)`,
+                      width: `${percentage}%`,
+                      backgroundColor:
+                        colorMapping[categories[index]] || "#ccc",
+                      cursor: "pointer",
+                    }}
+                  ></div>
+                );
+              })}
+            </div>
+            <div className="absolute top-[-5px] z-[-3] left-[-5px] flex h-[calc(100%_+_10px)] w-[calc(100%_+_10px)]">
+              {percentages.map((percentage, index) => {
+                const leftOffset = percentages
+                  .slice(0, index)
+                  .reduce((sum, pct) => sum + pct, 0);
+                return (
+                  <div
+                    key={index}
+                    className={`asbolute top-0 h-full transition-opacity duration-200 first:rounded-l-[20px] last:rounded-r-[20px]`}
+                    style={{
+                      left: `calc(${leftOffset}% - 5px)`,
+                      width: `${percentage}%`,
+                      backgroundColor:
+                        colorMapping[categories[index]] || "#ccc",
+                      cursor: "pointer",
+                    }}
+                  ></div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
