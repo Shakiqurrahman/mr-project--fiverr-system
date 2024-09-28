e3edf3cf5b0cd5da8a470cb19710968e02ef541c
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-09-29 00:51:49 +0600

    config updated

--- src/libs/configApi.js
+++ src/libs/configApi.js
@@ -1,7 +1,7 @@
 export const configApi = {
-  api: "http://localhost:3000/api/v1/",
+  // api: "http://localhost:3000/api/v1/",
   // api: "https://server.mahfujurrahm535.com/api/v1/"
-  // api: "https://dev.mahfujurrahm535.com/api/v1/"
+  api: "https://dev.mahfujurrahm535.com/api/v1/",
   
   socket : 'http://localhost:3000',
   // socket : 'https://dev.mahfujurrahm535.com'
