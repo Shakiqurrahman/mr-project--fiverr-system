8a1f177e6ffdaf4c0f43c84829c8b4895d8c6d82
shakiqurrahman <rahmanshakiqur@gmail.com>
2024-10-02 19:02:08 +0600

    loader added in profile layout

--- src/pages/ProfileLayout.jsx
+++ src/pages/ProfileLayout.jsx
@@ -3,22 +3,27 @@ import { useParams } from "react-router-dom";
 import { useFetchAllUsersQuery } from "../Redux/api/allUserApiSlice";
 import ErrorPage from "./ErrorPage";
 import Profile from "./Profile";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const ProfileLayout = () => {
   const { userName } = useParams();
-  const { data: usersData, refetch } = useFetchAllUsersQuery();
+  const { data: usersData, refetch, isLoading } = useFetchAllUsersQuery();
   const user = usersData?.find(
     (user) =>
       user.userName.toLowerCase().trim() === userName.toLowerCase().trim(),
   );
-  
+
   useEffect(() => {
     if (user) {
       refetch();
     }
   }, [user, refetch]);
 
-  return user ? (
+  return isLoading ? (
+    <div className="min-h-[calc(100vh_-_136px)] max-width flex justify-center items-center">
+      <AiOutlineLoading3Quarters  className="text-4xl animate-spin"/>
+    </div>
+  ) : user ? (
     <Profile user={user} slug={userName} />
   ) : (
     <ErrorPage />
