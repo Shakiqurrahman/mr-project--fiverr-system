927a56e250784b2364418f00f3a4ad9498a475de
Developer Shakil <itsdevelopershakil@gmail.com>
2024-10-12 16:13:00 +0600

    testimonial design fixed

--- src/pages/AllReviews.jsx
+++ src/pages/AllReviews.jsx
@@ -134,12 +134,12 @@ const AllReviews = () => {
                   <IoStar />
                   <IoStar />
                 </div>
-                <span className="text-lg font-semibold sm:text-2xl">(00)</span>
+                <span className="text-lg sm:text-2xl">(00)</span>
               </div>
             </div>
             <div className="px-2 lg:pr-4">
               <div className="rounded-2xl bg-white p-5 text-center">
-                <h1 className="text-lg font-semibold sm:text-2xl">2 Star</h1>
+                <h1 className="text-lg font-semibold sm:text-2xl">2 Stars</h1>
                 <div className="my-2 flex justify-center gap-3 text-lg text-[#C8E3F6] sm:my-5 sm:text-3xl">
                   <IoStar className="text-primary" />
                   <IoStar className="text-primary" />
@@ -147,12 +147,12 @@ const AllReviews = () => {
                   <IoStar />
                   <IoStar />
                 </div>
-                <span className="text-lg font-semibold sm:text-2xl">(01)</span>
+                <span className="text-lg sm:text-2xl">(01)</span>
               </div>
             </div>
             <div className="px-2 lg:pr-4">
               <div className="rounded-2xl bg-white p-5 text-center">
-                <h1 className="text-lg font-semibold sm:text-2xl">3 Star</h1>
+                <h1 className="text-lg font-semibold sm:text-2xl">3 Stars</h1>
                 <div className="my-2 flex justify-center gap-3 text-lg text-[#C8E3F6] sm:my-5 sm:text-3xl">
                   <IoStar className="text-primary" />
                   <IoStar className="text-primary" />
@@ -160,12 +160,12 @@ const AllReviews = () => {
                   <IoStar />
                   <IoStar />
                 </div>
-                <span className="text-lg font-semibold sm:text-2xl">(04)</span>
+                <span className="text-lg sm:text-2xl">(04)</span>
               </div>
             </div>
             <div className="px-2 lg:pr-4">
               <div className="rounded-2xl bg-white p-5 text-center">
-                <h1 className="text-lg font-semibold sm:text-2xl">4 Star</h1>
+                <h1 className="text-lg font-semibold sm:text-2xl">4 Stars</h1>
                 <div className="my-2 flex justify-center gap-3 text-lg text-[#C8E3F6] sm:my-5 sm:text-3xl">
                   <IoStar className="text-primary" />
                   <IoStar className="text-primary" />
@@ -173,12 +173,12 @@ const AllReviews = () => {
                   <IoStar className="text-primary" />
                   <IoStar />
                 </div>
-                <span className="text-lg font-semibold sm:text-2xl">(02)</span>
+                <span className="text-lg sm:text-2xl">(02)</span>
               </div>
             </div>
             <div className="px-2 lg:pr-0">
               <div className="rounded-2xl bg-white p-5 text-center">
-                <h1 className="text-lg font-semibold sm:text-2xl">5 Star</h1>
+                <h1 className="text-lg font-semibold sm:text-2xl">5 Stars</h1>
                 <div className="my-2 flex justify-center gap-3 text-lg text-[#C8E3F6] sm:my-5 sm:text-3xl">
                   <IoStar className="text-primary" />
                   <IoStar className="text-primary" />
@@ -186,14 +186,14 @@ const AllReviews = () => {
                   <IoStar className="text-primary" />
                   <IoStar className="text-primary" />
                 </div>
-                <span className="text-lg font-semibold sm:text-2xl">(24)</span>
+                <span className="text-lg sm:text-2xl">(24)</span>
               </div>
             </div>
           </Slider>
         </div>
         <div className="mt-10 flex flex-wrap-reverse items-center gap-y-5 md:flex-nowrap md:gap-0">
           <div className="flex w-full flex-wrap items-center justify-start gap-x-3 gap-y-4 md:w-3/4">
-            <h1 className="text-sm font-semibold sm:text-base lg:order-none lg:text-2xl">
+            <h1 className="text-sm font-semibold sm:text-base lg:order-none lg:text-xl">
               Sort By
             </h1>
 
@@ -201,7 +201,7 @@ const AllReviews = () => {
               value="Most relevant"
               onClick={handleSortBtn}
               type="button"
-              className={`sortingBtn rounded border border-transparent p-1 text-xs font-medium sm:px-3 sm:py-2 lg:order-none lg:mx-0 lg:text-base ${sortBtn === "Most relevant" ? "active" : ""}`}
+              className={`sortingBtn rounded border border-transparent p-1 text-xs font-medium sm:px-3 sm:py-2 lg:order-none lg:mx-0 lg:text-lg ${sortBtn === "Most relevant" ? "active" : ""}`}
             >
               Most relevant
             </button>
@@ -210,7 +210,7 @@ const AllReviews = () => {
               value="Most recent"
               onClick={handleSortBtn}
               type="button"
-              className={`sortingBtn rounded border border-transparent p-1 text-xs font-medium sm:px-3 sm:py-2 lg:order-none lg:text-base ${sortBtn === "Most recent" ? "active" : ""}`}
+              className={`sortingBtn rounded border border-transparent p-1 text-xs font-medium sm:px-3 sm:py-2 lg:order-none lg:text-lg ${sortBtn === "Most recent" ? "active" : ""}`}
             >
               Most recent
             </button>
@@ -218,7 +218,7 @@ const AllReviews = () => {
               value="Delivery images"
               onClick={handleSortBtn}
               type="button"
-              className={`sortingBtn rounded border border-transparent p-1 text-xs font-medium sm:px-3 sm:py-2 lg:order-none lg:text-base ${sortBtn === "Delivery images" ? "active" : ""}`}
+              className={`sortingBtn rounded border border-transparent p-1 text-xs font-medium sm:px-3 sm:py-2 lg:order-none lg:text-lg ${sortBtn === "Delivery images" ? "active" : ""}`}
             >
               Delivery images (23)
             </button>
@@ -242,47 +242,48 @@ const AllReviews = () => {
             <div className="mb-5">
               <div className="">
                 <div className="flex flex-col-reverse items-center gap-4 sm:flex-row sm:items-start">
-                  <p className="text-center text-base leading-[40px] sm:text-start md:text-lg">
-                    <span>
-                      <FaQuoteLeft className="mb-3 mr-1 inline text-xs text-red-500" />
-                    </span>
-                    {review.reviewText}
-                    <span>
-                      <FaQuoteRight className="mb-3 ml-1 inline text-xs text-red-500" />
-                    </span>
-                  </p>
+                  <div>
+                    <p className="text-center text-base leading-[40px] sm:text-start md:text-lg">
+                      <span>
+                        <FaQuoteLeft className="mb-3 mr-1 inline text-xs text-red-500" />
+                      </span>
+                      {review.reviewText}
+                      <span>
+                        <FaQuoteRight className="mb-3 ml-1 inline text-xs text-red-500" />
+                      </span>
+                    </p>
+                    <div className="mt-5 flex flex-col items-center gap-2 sm:gap-4 lg:flex-row">
+                      <img
+                        src={review.user.image}
+                        alt=""
+                        className="h-[30px] w-[30px] rounded-full sm:h-[40px] sm:w-[40px]"
+                      />
+                      <h1 className="text-base font-semibold md:text-xl">
+                        {review.user.userName}
+                      </h1>
+                      <div className="ml-0 flex justify-center gap-2 text-lg text-[#C8E3F6] md:text-2xl lg:ml-3">
+                        <IoStar className="text-primary" />
+                        <IoStar className="text-primary" />
+                        <IoStar className="text-primary" />
+                        <IoStar className="text-primary" />
+                        <IoStar className="text-primary" />
+                      </div>
+                      <p className="ml-0 text-base md:text-lg lg:ml-3">
+                        {review.user.country}
+                      </p>
+                      <p className="mt-2 text-xs md:text-base lg:mt-0">
+                        5 days ago
+                      </p>
+                    </div>
+                  </div>
+
                   {review.thumbnail && (
                     <img
                       src={review.thumbnail}
                       alt=""
-                      className="h-[150px] w-[150px] rounded-xl object-[100%_100%]"
+                      className="w-[150px] rounded-xl object-cover"
                     />
                   )}
-                </div>
-                <div className="mt-10 flex flex-col items-center justify-between lg:flex-row">
-                  <div className="flex flex-col items-center gap-2 sm:gap-4 lg:flex-row">
-                    <img
-                      src={review.user.image}
-                      alt=""
-                      className="h-[30px] w-[30px] rounded-full sm:h-[40px] sm:w-[40px]"
-                    />
-                    <h1 className="text-base font-semibold md:text-xl">
-                      {review.user.userName}
-                    </h1>
-                    <div className="ml-0 flex justify-center gap-2 text-lg text-[#C8E3F6] md:text-2xl lg:ml-3">
-                      <IoStar className="text-primary" />
-                      <IoStar className="text-primary" />
-                      <IoStar className="text-primary" />
-                      <IoStar className="text-primary" />
-                      <IoStar className="text-primary" />
-                    </div>
-                    <p className="ml-0 text-base md:text-lg lg:ml-3">
-                      {review.user.country}
-                    </p>
-                  </div>
-                  <p className="mt-2 text-xs md:text-base lg:mt-0">
-                    5 days ago
-                  </p>
                 </div>
               </div>
             </div>
