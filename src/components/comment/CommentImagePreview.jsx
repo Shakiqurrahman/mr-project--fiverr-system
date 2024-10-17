b3477bbe99dc2423e302e59e477a69b502e84349
Developer Shakil <itsdevelopershakil@gmail.com>
2024-10-18 00:22:22 +0600

    added image marker into commentimagepreview

--- src/components/comment/CommentImagePreview.jsx
+++ src/components/comment/CommentImagePreview.jsx
@@ -1,10 +1,54 @@
+import { useState } from "react";
 import { IoMdArrowBack } from "react-icons/io";
 import { LiaDownloadSolid } from "react-icons/lia";
+import ImageMarker from "react-image-marker";
 import { useSelector } from "react-redux";
+import thumbnail2 from "../../assets/images/project-thumbnail-alt.jpg";
+import thumbnail from "../../assets/images/project-thumbnail.jpg";
+import formatFileSize from "../../libs/formatFileSize";
 
 const CommentImagePreview = () => {
-  const multiple = true;
   const { user } = useSelector((state) => state.user);
+  const images = [
+    {
+      id: 1,
+      url: thumbnail,
+      name: "Image 1.jpg",
+      size: 215463,
+    },
+    {
+      id: 2,
+      url: thumbnail2,
+      name: "Image 2.jpg",
+      size: 2154631,
+    },
+  ];
+
+  const [markers, setMarkers] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(images[0]);
+
+  const CustomMarker = () => {
+    return (
+      <p
+        className="size-5 rounded-full bg-primary"
+        onClick={() => console.log("clicked")}
+      ></p>
+    );
+  };
+
+  const handleMarkerAdd = (marker) => {
+    const id = markers.length + 1;
+    const newMarker = { id, ...marker };
+    setMarkers([...markers, newMarker]);
+  };
+
+  // const handleMarkerDelete = (id) => {
+  //   const newMarkers = markers.filter((m) => m.id !== id);
+  //   setMarkers(newMarkers);
+  // };
+
+  const multiple = images.length > 1;
+
   return (
     <div className="h-full w-full">
       <div className="flex h-[60px] items-center gap-3 p-4 font-medium text-white">
@@ -15,12 +59,14 @@ const CommentImagePreview = () => {
           <IoMdArrowBack className="text-xl" />
         </button>
         <p>
-          Cleaning Services Flyer Design 2.jpg{" "}
-          <span className="text-white/50">(2.62 MB)</span>
+          {selectedImage?.name}{" "}
+          <span className="text-white/50">
+            ({formatFileSize(selectedImage?.size)})
+          </span>
         </p>
-        <button type="button">
+        <a href={selectedImage?.url} download={selectedImage?.name}>
           <LiaDownloadSolid className="text-xl text-primary" />
-        </button>
+        </a>
         <div className="ms-auto flex gap-2">
           {user?.image ? (
             <img
@@ -49,28 +95,32 @@ const CommentImagePreview = () => {
       <div
         className={`flex ${multiple ? "h-[calc(100%_-_160px)]" : "h-[calc(100%_-_60px)]"} w-full items-center justify-center p-10`}
       >
-        <img
-          src={user?.image}
-          alt=""
-          className="max-h-full max-w-full object-contain"
+        <ImageMarker
+          src={selectedImage?.url} // Replace with your image URL
+          markers={markers}
+          onAddMarker={handleMarkerAdd}
+          markerComponent={CustomMarker}
+          extraClass="max-h-full max-w-full object-contain"
         />
       </div>
-      <div className="preview-scroll-overflow-x flex h-[100px] w-full items-center gap-3 p-5">
-        <button type="button" className="h-full">
-          <img
-            src={user?.image}
-            alt=""
-            className="max-h-full w-full object-contain"
-          />
-        </button>
-        <button type="button" className="h-full">
-          <img
-            src={user?.image}
-            alt=""
-            className="max-h-full w-full object-contain"
-          />
-        </button>
-      </div>
+      {multiple && (
+        <div className="preview-scroll-overflow-x flex h-[100px] w-full items-center gap-3 p-5">
+          {images?.map((img, index) => (
+            <button
+              key={index}
+              type="button"
+              className="h-full"
+              onClick={() => setSelectedImage(img)}
+            >
+              <img
+                src={img?.url}
+                alt={img?.name}
+                className="max-h-full w-full object-contain"
+              />
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
