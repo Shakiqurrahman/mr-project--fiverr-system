f0a6a912f64d73f151f3702a8a7b4e20794a648a
Developer Shakil <itsdevelopershakil@gmail.com>
2024-10-25 20:04:44 +0600

    searchbox component added into sitemap design search button

--- src/components/partials/SearchBox.jsx
+++ src/components/partials/SearchBox.jsx
@@ -16,7 +16,7 @@ const SearchBox = ({ handleClose }) => {
 
   return (
     <div className="fixed left-0 top-0 z-[99999] flex h-screen w-full items-start justify-center overflow-y-auto bg-black/80 px-5 py-10 backdrop-blur-sm">
-      <div className="w-full rounded-lg bg-white p-3">
+      <div className="w-full max-w-[600px] rounded-lg bg-white p-3">
         <div className="flex items-center gap-2 rounded-lg border-2 border-primary p-3">
           <IoSearch className="shrink-0 text-2xl text-gray-400" />
           <input
--- src/components/sitemap/SitemapDesign.jsx
+++ src/components/sitemap/SitemapDesign.jsx
@@ -1,148 +1,154 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import SearchBox from "../partials/SearchBox";
 
 const SitemapDesign = () => {
+  const [openSearchBox, setOpenSearchBox] = useState(false);
   return (
-    <div className="my-10">
-      <h1 className="mb-10 text-xl font-semibold text-revision">
-        Homepage Designs
-      </h1>
-      <div className="ml-5">
-        <ul className="list-disc marker:text-xl marker:text-revision">
-          <li>
-            <Link
-              className="text-lg font-semibold text-primary hover:underline"
-              to={"/"}
-            >
-              Boxes
-            </Link>
-            <p className="mb-10 mt-3">
-              There are different boxes on the homepage for different designs,
-              you can see the design you need from the specific box.
-            </p>
-          </li>
-        </ul>
-        <ul className="ml-10 list-disc marker:text-xl marker:text-revision">
-          <li>
-            <Link
-              className="text-lg font-semibold text-primary hover:underline"
-              to={"/"}
-            >
-              Folders
-            </Link>
-            <p className="mb-10 mt-3">
-              There are different folders in each design box, and each folder is
-              for different industry designs, you can find the folder you need
-              by clicking the &quot;Arrow Icon&quot; in the box, or you can see
-              all the folders together by clicking &quot;See More&quot; on the
-              top right side of the box.
-            </p>
-          </li>
-        </ul>
-        <ul className="ml-20 list-disc marker:text-xl marker:text-revision">
-          <li>
-            <Link
-              className="text-lg font-semibold text-primary hover:underline"
-              to={"/"}
-            >
-              Industries
-            </Link>
-            <div className="mb-10 mt-3">
-              Find your specific industry or similar industry by checking the
-              title below each design and click on the selected industry to view
-              the designs within the folder.
-              <p>
-                (If you can&apos;t find your specific or similar industry,
-                please inbox us)
+    <>
+      <div className="my-10">
+        <h1 className="mb-10 text-xl font-semibold text-revision">
+          Homepage Designs
+        </h1>
+        <div className="ml-5">
+          <ul className="list-disc marker:text-xl marker:text-revision">
+            <li>
+              <Link
+                className="text-lg font-semibold text-primary hover:underline"
+                to={"/"}
+              >
+                Boxes
+              </Link>
+              <p className="mb-10 mt-3">
+                There are different boxes on the homepage for different designs,
+                you can see the design you need from the specific box.
               </p>
-            </div>
-          </li>
-        </ul>
-        <ul className="ml-[120px] list-disc marker:text-xl marker:text-revision">
-          <li>
-            <Link
-              className="text-lg font-semibold text-primary hover:underline"
-              to={"/"}
-            >
-              Designs
-            </Link>
-            <p className="mb-10 mt-3">
-              Click on the design to view the design of your choice better and
-              view the design information.
-            </p>
-          </li>
-        </ul>
-      </div>
+            </li>
+          </ul>
+          <ul className="ml-10 list-disc marker:text-xl marker:text-revision">
+            <li>
+              <Link
+                className="text-lg font-semibold text-primary hover:underline"
+                to={"/"}
+              >
+                Folders
+              </Link>
+              <p className="mb-10 mt-3">
+                There are different folders in each design box, and each folder
+                is for different industry designs, you can find the folder you
+                need by clicking the &quot;Arrow Icon&quot; in the box, or you
+                can see all the folders together by clicking &quot;See
+                More&quot; on the top right side of the box.
+              </p>
+            </li>
+          </ul>
+          <ul className="ml-20 list-disc marker:text-xl marker:text-revision">
+            <li>
+              <Link
+                className="text-lg font-semibold text-primary hover:underline"
+                to={"/"}
+              >
+                Industries
+              </Link>
+              <div className="mb-10 mt-3">
+                Find your specific industry or similar industry by checking the
+                title below each design and click on the selected industry to
+                view the designs within the folder.
+                <p>
+                  (If you can&apos;t find your specific or similar industry,
+                  please inbox us)
+                </p>
+              </div>
+            </li>
+          </ul>
+          <ul className="ml-[120px] list-disc marker:text-xl marker:text-revision">
+            <li>
+              <Link
+                className="text-lg font-semibold text-primary hover:underline"
+                to={"/"}
+              >
+                Designs
+              </Link>
+              <p className="mb-10 mt-3">
+                Click on the design to view the design of your choice better and
+                view the design information.
+              </p>
+            </li>
+          </ul>
+        </div>
 
-      <h1 className="mb-10 text-xl font-semibold text-revision">Others</h1>
+        <h1 className="mb-10 text-xl font-semibold text-revision">Others</h1>
 
-      <div className="ml-5">
-        <ul className="list-disc marker:text-xl marker:text-revision">
-          <li>
-            <Link
-              className="text-lg font-semibold text-primary hover:underline"
-              to={"/"}
-            >
-              Search
-            </Link>
-            <p className="mb-10 mt-3">
-              You can search for the design you need from the search box of the
-              top bar.
-            </p>
-          </li>
-        </ul>
-        <ul className="list-disc marker:text-xl marker:text-revision">
-          <li>
-            <Link
-              className="text-lg font-semibold text-primary hover:underline"
-              to={"/designs"}
-            >
-              Designs
-            </Link>
-            <div className="mb-10 mt-3">
-              Click on &quot;Designs&quot; from the top bar or footer, then
-              select the design tag you need, and then select your specific or
-              similar industry tag. Then see your selected designs below.
-              <p>
-                (if you don&apos;t find your preferred design please inbox us)
+        <div className="ml-5">
+          <ul className="list-disc marker:text-xl marker:text-revision">
+            <li>
+              <Link
+                className="text-lg font-semibold text-primary hover:underline"
+                onClick={() => setOpenSearchBox(true)}
+              >
+                Search
+              </Link>
+              <p className="mb-10 mt-3">
+                You can search for the design you need from the search box of
+                the top bar.
               </p>
-            </div>
-          </li>
-        </ul>
-        <ul className="list-disc marker:text-xl marker:text-revision">
-          <li>
-            <Link
-              className="text-lg font-semibold text-primary hover:underline"
-              to={"/industries"}
-            >
-              Industries
-            </Link>
-            <div className="mb-10 mt-3">
-              Click on &quot;Industries&quot; from the top bar or footer, then
-              select your specific or similar industry tag, and then select the
-              design tag you need. Then see your selected designs below.
-              <p>
-                (if you don&apos;t find your preferred design please inbox us)
+            </li>
+          </ul>
+          <ul className="list-disc marker:text-xl marker:text-revision">
+            <li>
+              <Link
+                className="text-lg font-semibold text-primary hover:underline"
+                to={"/designs"}
+              >
+                Designs
+              </Link>
+              <div className="mb-10 mt-3">
+                Click on &quot;Designs&quot; from the top bar or footer, then
+                select the design tag you need, and then select your specific or
+                similar industry tag. Then see your selected designs below.
+                <p>
+                  (if you don&apos;t find your preferred design please inbox us)
+                </p>
+              </div>
+            </li>
+          </ul>
+          <ul className="list-disc marker:text-xl marker:text-revision">
+            <li>
+              <Link
+                className="text-lg font-semibold text-primary hover:underline"
+                to={"/industries"}
+              >
+                Industries
+              </Link>
+              <div className="mb-10 mt-3">
+                Click on &quot;Industries&quot; from the top bar or footer, then
+                select your specific or similar industry tag, and then select
+                the design tag you need. Then see your selected designs below.
+                <p>
+                  (if you don&apos;t find your preferred design please inbox us)
+                </p>
+              </div>
+            </li>
+          </ul>
+          <ul className="list-disc marker:text-xl marker:text-revision">
+            <li>
+              <Link
+                className="text-lg font-semibold text-primary hover:underline"
+                to={"/"}
+              >
+                Completed Projects
+              </Link>
+              <p className="mb-10 mt-3">
+                At the bottom of the home page there is a box called
+                &quot;Completed Projects&quot;, from there you can see some of
+                our previous completed project designs.
               </p>
-            </div>
-          </li>
-        </ul>
-        <ul className="list-disc marker:text-xl marker:text-revision">
-          <li>
-            <Link
-              className="text-lg font-semibold text-primary hover:underline"
-              to={"/"}
-            >
-              Completed Projects
-            </Link>
-            <p className="mb-10 mt-3">
-              At the bottom of the home page there is a box called
-              &quot;Completed Projects&quot;, from there you can see some of our
-              previous completed project designs.
-            </p>
-          </li>
-        </ul>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+      {openSearchBox && <SearchBox handleClose={setOpenSearchBox} />}
+    </>
   );
 };
 
