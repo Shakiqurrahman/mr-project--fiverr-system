5c6622f0a517eb29c94f15a2354abe86967e21f1
Developer Shakil <itsdevelopershakil@gmail.com>
2024-11-08 01:07:39 +0600

    authwrapper useeffect commented

--- src/libs/AuthWrapper.jsx
+++ src/libs/AuthWrapper.jsx
@@ -2,9 +2,8 @@ import Cookies from "js-cookie";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useFetchUserDataQuery } from "../Redux/api/apiSlice";
-import { logout, setOnlineUsers, setUser } from "../Redux/features/userSlice";
-import { configApi } from "./configApi";
-import { connectSocket, disconnectSocket } from "./socketService";
+import { logout, setUser } from "../Redux/features/userSlice";
+import { disconnectSocket } from "./socketService";
 
 const AuthWrapper = ({ children }) => {
   const dispatch = useDispatch();
@@ -25,30 +24,30 @@ const AuthWrapper = ({ children }) => {
     }
   }, [user, error, dispatch]);
 
-  const token = Cookies.get("authToken");
-  const socket = connectSocket(`${configApi.socket}`, token);
+  // const token = Cookies.get("authToken");
+  // const socket = connectSocket(`${configApi.socket}`, token);
   // all avaiable user's
-  useEffect(() => {
-    const fetchOnlineUsers = () => {
-      socket?.emit("view-online-users");
-      socket?.on("online-users", (onlineUsers) => {
-        dispatch(setOnlineUsers(onlineUsers));
-      });
-    };
+  // useEffect(() => {
+  //   const fetchOnlineUsers = () => {
+  //     socket?.emit("view-online-users");
+  //     socket?.on("online-users", (onlineUsers) => {
+  //       dispatch(setOnlineUsers(onlineUsers));
+  //     });
+  //   };
 
-    // Fetch immediately on mount
-    fetchOnlineUsers();
+  //   // Fetch immediately on mount
+  //   fetchOnlineUsers();
 
-    const intervalId = setInterval(() => {
-      fetchOnlineUsers();
-    }, 1000 * 60);
+  //   const intervalId = setInterval(() => {
+  //     fetchOnlineUsers();
+  //   }, 1000 * 60);
 
-    // Clean up the interval on unmount to prevent memory leaks
-    return () => {
-      clearInterval(intervalId);
-      socket?.off("online-users");
-    };
-  }, [socket, dispatch]);
+  //   // Clean up the interval on unmount to prevent memory leaks
+  //   return () => {
+  //     clearInterval(intervalId);
+  //     socket?.off("online-users");
+  //   };
+  // }, [socket, dispatch]);
 
   return children;
 };
