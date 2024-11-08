29b0b95fd0790f080d4908698f5c931df97e4c49
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-11-09 01:43:08 +0600

    tracking project integrate done

--- src/components/order/sidebar-components/TrackProject.jsx
+++ src/components/order/sidebar-components/TrackProject.jsx
@@ -1,42 +1,59 @@
 import React from "react";
 import { FaCheck } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 const TrackProject = () => {
+  const { projectDetails } = useSelector((state) => state.order);
   const trackingOptions = [
     {
       id: 1,
       name: "Project placed",
-      status: "Completed",
     },
     {
       id: 2,
       name: "Requirements submitted",
-      status: "Completed",
     },
     {
       id: 3,
       name: "Project running",
-      status: "Ongoing",
     },
     {
       id: 4,
       name: "Review Delivery",
-      status: "Waiting",
     },
     {
       id: 5,
       name: "Complete project",
-      status: "Waiting",
     },
   ];
+
+  const statusMapping = {
+    PROJECT_PLACED: "Project placed",
+    REQUIREMENTS_SUBMITTED: "Requirements submitted",
+    PROJECT_RUNNING: "Project running",
+    REVIEW_DELIVERY: "Review Delivery",
+    COMPLETE_PROJECT: "Complete project",
+  };
+
+  const currentStatusIndex = trackingOptions.findIndex(
+    (option) =>
+      option.name === statusMapping[projectDetails?.trackProjectStatus],
+  );
+
   return (
     <div className="bg-lightskyblue p-3">
       <h1 className="text-xl font-semibold">Track Project</h1>
       <div className="my-5 space-y-6">
         {trackingOptions.map((option, idx) => {
+          const status =
+            idx <= currentStatusIndex
+              ? "Completed"
+              : idx === currentStatusIndex + 1
+                ? "Ongoing"
+                : "Waiting";
           return (
             <div key={idx} className="flex items-center gap-4">
-              {option?.status === "Waiting" ? (
+              {status === "Waiting" ? (
                 <span className="relative flex size-7 flex-shrink-0 items-center justify-center rounded-full border-2 border-gray-400 bg-transparent p-1 text-base text-white">
                   {idx !== 0 && (
                     <div className="absolute bottom-full left-1/2 h-[26px] w-[2px] -translate-x-1/2 bg-gray-400"></div>
@@ -44,7 +61,7 @@ const TrackProject = () => {
                 </span>
               ) : (
                 <span className="relative flex size-7 flex-shrink-0 items-center justify-center rounded-full bg-primary p-1 text-base text-white">
-                  {option?.status === "Completed" && <FaCheck />}
+                  {status === "Completed" && <FaCheck />}
                   {idx !== 0 && (
                     <div className="absolute bottom-full left-1/2 h-[26px] w-[2px] -translate-x-1/2 bg-primary"></div>
                   )}
