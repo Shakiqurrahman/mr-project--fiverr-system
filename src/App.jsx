6d704f484c68ac097418cf281dfa93d564900bf8
Developer Shakil <itsdevelopershakil@gmail.com>
2024-11-15 22:56:28 +0600

    total visitor api added into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import axios from "axios";
+import { useEffect } from "react";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import Layout from "./AllRoutes/Layout";
 import { router } from "./AllRoutes/Routes";
 import store from "./Redux/store";
 import AuthWrapper from "./libs/AuthWrapper";
+import { configApi } from "./libs/configApi";
 function App() {
+  useEffect(() => {
+    const url = `${configApi.api}analytics/visitors/`;
+    axios.get(url);
+  }, []);
   return (
     <Provider store={store}>
       <AuthWrapper>
