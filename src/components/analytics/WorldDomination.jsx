1e7a70760803b526a8a5f28dacf5ca6fd16de9c4
Developer Shakil <itsdevelopershakil@gmail.com>
2024-11-17 18:52:27 +0600

    world domination done

--- src/components/analytics/WorldDomination.jsx
+++ src/components/analytics/WorldDomination.jsx
@@ -1,18 +1,18 @@
+import { useEffect, useState } from "react";
 import WorldMap from "react-svg-worldmap";
+import { useGetWordlDominationDataQuery } from "../../Redux/api/analyticsApiSlice";
 
 const WorldDomination = () => {
-  const data = [
-    { country: "cn", value: 1389618778 }, // china
-    { country: "in", value: 1311559204 }, // india
-    { country: "us", value: 331883986 }, // united states
-    { country: "id", value: 264935824 }, // indonesia
-    { country: "pk", value: 210797836 }, // pakistan
-    { country: "br", value: 210301591 }, // brazil
-    { country: "ng", value: 208679114 }, // nigeria
-    { country: "bd", value: 161062905 }, // bangladesh
-    { country: "ru", value: 141944641 }, // russia
-    { country: "mx", value: 127318112 }, // mexico
-  ];
+  const { data: worldsData } = useGetWordlDominationDataQuery();
+  console.log(worldsData);
+  const [dataList, setDataList] = useState([
+    // { country: "cn", value: 1389618778 }, // china
+  ]);
+  useEffect(() => {
+    if (worldsData) {
+      setDataList(worldsData.worldDominationDetails);
+    }
+  }, [worldsData]);
   const getStyle = ({ countryValue, color }) => ({
     fill: countryValue ? "rgba(27, 140, 220, 1)" : color,
     stroke: "#fff",
@@ -22,11 +22,13 @@ const WorldDomination = () => {
   });
   return (
     <div className="border p-3">
-      <h1 className="text-lg font-semibold">World Domination 9%</h1>
+      <h1 className="text-lg font-semibold">
+        World Domination {worldsData?.worldDomiation || "0%"}
+      </h1>
       <div>
         <WorldMap
           color="rgba(27, 140, 220, 0.2)"
-          data={data}
+          data={dataList}
           size={"xxl"}
           styleFunction={getStyle}
           valueSuffix="Sales"
