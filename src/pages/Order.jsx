030981e92ff860aac7353419d9b7e93d40d3fcb5
Developer Shakil <itsdevelopershakil@gmail.com>
2024-11-19 16:54:06 +0600

    order page conditional rendering done

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -101,108 +101,111 @@ const Order = () => {
   console.log("admin review", adminReview);
 
   return (
-    <div className="max-width my-10">
-      {projectDetails?.projectStatus === "Completed" && (
-        <h1 className="mx-auto mb-10 flex w-full items-center justify-center gap-3 rounded-[30px] bg-revision px-5 py-2 text-xl font-semibold text-white sm:w-1/2 md:text-3xl">
-          <Check className="size-6 !fill-white md:size-10" /> Project Completed{" "}
-        </h1>
-      )}
-      {projectDetails?.projectStatus === "Canceled" && (
-        <h1 className="mx-auto mb-10 flex w-full items-center justify-center gap-3 rounded-[30px] bg-canceled px-5 py-2 text-xl font-semibold text-white sm:w-1/2 md:text-3xl">
-          <MdOutlineDoNotDisturbAlt className="size-6 !text-white md:size-10" />
-          Project Canceled{" "}
-        </h1>
-      )}
-      <div className="relative flex flex-wrap gap-5 sm:flex-nowrap">
-        <div className="mb-5 w-full shrink lg:w-[calc(100%_-_320px)]">
-          <div className="flex items-center justify-between gap-2">
-            <div className="hidden items-center gap-10 sm:flex">
-              {tabButtons.map((btn, i) => (
-                <button
-                  key={i}
-                  type="button"
-                  className={`relative font-semibold ${selectedTabButton === btn ? "text-primary underline" : ""}`}
-                  value={btn}
-                  onClick={() => setSelectedTabButton(btn)}
-                >
-                  {btn}
-                  {btn === "REQUIREMENTS" &&
-                    !projectDetails?.isRequirementsFullFilled && (
-                      <div className="absolute -right-3 -top-2">
-                        <PiWarningCircleFill className="text-primary" />
-                      </div>
-                    )}
-                </button>
-              ))}
-            </div>
-            <select
-              value={selectedTabButton}
-              onChange={(e) => setSelectedTabButton(e.target.value)}
-              className="block w-full border border-gray-300 p-3 font-medium sm:hidden"
-            >
-              {tabButtons.map((btn, i) => (
-                <option key={i} value={btn}>
-                  {btn}{" "}
-                </option>
-              ))}
-            </select>
-            <button
-              className="block lg:hidden"
-              onClick={() => setOpenSidePanel(true)}
-            >
-              <GiHamburgerMenu className="text-xl" />
-            </button>
-          </div>
-          <Divider className="my-5 h-px w-full !bg-black" />
-          <RenderTabComponent />
-        </div>
-        <div
-          className={`${openSidePanel ? "absolute right-0 top-0 !block" : ""} hidden w-[300px] shrink-0 bg-white lg:block`}
-        >
-          <button
-            className="absolute right-4 top-4 z-10 block lg:hidden"
-            onClick={() => setOpenSidePanel(false)}
-          >
-            <IoClose className="text-2xl" />
-          </button>
-          <OrderSidePanel />
-        </div>
-      </div>
-      {selectedTabButton === "ACTIVITY" &&
-        projectDetails?.projectStatus === "Completed" && (
-          <div className="w-full lg:w-[calc(100%_-_320px)]">
-            {clientReview && isAdmin && (
-              <OrderReview reviewDetails={clientReview} />
-            )}
-            {adminReview && user?.role === "USER" && (
-              <OrderReview reviewDetails={adminReview} />
-            )}
-            {projectDetails?.review?.length === 0 && user?.role === "USER" ? (
-              <OrderReviewForm />
-            ) : projectDetails?.review?.length === 1 && isAdmin ? (
-              <OrderReviewForm />
-            ) : null}
-            {clientReview &&
-              user?.role === "USER" &&
-              !projectDetails?.projectTips && <OrderTipsForm />}
-            {user?.role === "USER" ? (
-              <p className="mt-5 text-center text-lg font-semibold">
-                Your project is complete. If you need to contact the seller,{" "}
-                <Link to={"/inbox"} className="text-primary underline">
-                  Go to Inbox
-                </Link>
-              </p>
-            ) : (
-              <p className="mt-5 text-center text-lg font-semibold">
-                Your project is complete. If you need to contact the buyer,{" "}
-                <Link to={"/inbox"} className="text-primary underline">
-                  Go to Inbox
-                </Link>
-              </p>
-            )}
-          </div>
+    projectDetails && (
+      <div className="max-width my-10">
+        {projectDetails?.projectStatus === "Completed" && (
+          <h1 className="mx-auto mb-10 flex w-full items-center justify-center gap-3 rounded-[30px] bg-revision px-5 py-2 text-xl font-semibold text-white sm:w-1/2 md:text-3xl">
+            <Check className="size-6 !fill-white md:size-10" /> Project
+            Completed{" "}
+          </h1>
         )}
-    </div>
+        {projectDetails?.projectStatus === "Canceled" && (
+          <h1 className="mx-auto mb-10 flex w-full items-center justify-center gap-3 rounded-[30px] bg-canceled px-5 py-2 text-xl font-semibold text-white sm:w-1/2 md:text-3xl">
+            <MdOutlineDoNotDisturbAlt className="size-6 !text-white md:size-10" />
+            Project Canceled{" "}
+          </h1>
+        )}
+        <div className="relative flex flex-wrap gap-5 sm:flex-nowrap">
+          <div className="mb-5 w-full shrink lg:w-[calc(100%_-_320px)]">
+            <div className="flex items-center justify-between gap-2">
+              <div className="hidden items-center gap-10 sm:flex">
+                {tabButtons.map((btn, i) => (
+                  <button
+                    key={i}
+                    type="button"
+                    className={`relative font-semibold ${selectedTabButton === btn ? "text-primary underline" : ""}`}
+                    value={btn}
+                    onClick={() => setSelectedTabButton(btn)}
+                  >
+                    {btn}
+                    {btn === "REQUIREMENTS" &&
+                      !projectDetails?.isRequirementsFullFilled && (
+                        <div className="absolute -right-3 -top-2">
+                          <PiWarningCircleFill className="text-primary" />
+                        </div>
+                      )}
+                  </button>
+                ))}
+              </div>
+              <select
+                value={selectedTabButton}
+                onChange={(e) => setSelectedTabButton(e.target.value)}
+                className="block w-full border border-gray-300 p-3 font-medium sm:hidden"
+              >
+                {tabButtons.map((btn, i) => (
+                  <option key={i} value={btn}>
+                    {btn}{" "}
+                  </option>
+                ))}
+              </select>
+              <button
+                className="block lg:hidden"
+                onClick={() => setOpenSidePanel(true)}
+              >
+                <GiHamburgerMenu className="text-xl" />
+              </button>
+            </div>
+            <Divider className="my-5 h-px w-full !bg-black" />
+            <RenderTabComponent />
+          </div>
+          <div
+            className={`${openSidePanel ? "absolute right-0 top-0 !block" : ""} hidden w-[300px] shrink-0 bg-white lg:block`}
+          >
+            <button
+              className="absolute right-4 top-4 z-10 block lg:hidden"
+              onClick={() => setOpenSidePanel(false)}
+            >
+              <IoClose className="text-2xl" />
+            </button>
+            <OrderSidePanel />
+          </div>
+        </div>
+        {selectedTabButton === "ACTIVITY" &&
+          projectDetails?.projectStatus === "Completed" && (
+            <div className="w-full lg:w-[calc(100%_-_320px)]">
+              {clientReview && isAdmin && (
+                <OrderReview reviewDetails={clientReview} />
+              )}
+              {adminReview && user?.role === "USER" && (
+                <OrderReview reviewDetails={adminReview} />
+              )}
+              {projectDetails?.review?.length === 0 && user?.role === "USER" ? (
+                <OrderReviewForm />
+              ) : projectDetails?.review?.length === 1 && isAdmin ? (
+                <OrderReviewForm />
+              ) : null}
+              {clientReview &&
+                user?.role === "USER" &&
+                !projectDetails?.projectTips && <OrderTipsForm />}
+              {user?.role === "USER" ? (
+                <p className="mt-5 text-center text-lg font-semibold">
+                  Your project is complete. If you need to contact the seller,{" "}
+                  <Link to={"/inbox"} className="text-primary underline">
+                    Go to Inbox
+                  </Link>
+                </p>
+              ) : (
+                <p className="mt-5 text-center text-lg font-semibold">
+                  Your project is complete. If you need to contact the buyer,{" "}
+                  <Link to={"/inbox"} className="text-primary underline">
+                    Go to Inbox
+                  </Link>
+                </p>
+              )}
+            </div>
+          )}
+      </div>
+    )
   );
 };
 
