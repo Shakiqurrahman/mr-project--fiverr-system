ef732a7a01931b8db052dcae4012ec92cfae93ea
Developer Shakil <itsdevelopershakil@gmail.com>
2024-12-12 00:24:41 +0600

    preview image size fixed

--- src/components/PreviewImage.jsx
+++ src/components/PreviewImage.jsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import { MdClose } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import { closePreviewImage } from "../Redux/features/previewImageSlice";
 import useOutsideClick from "../hooks/useOutsideClick";
@@ -12,11 +13,17 @@ const PreviewImage = () => {
   };
   useOutsideClick(imageRef, closePreview);
   return (
-    <div className="fixed left-0 top-0 z-[9999999] flex h-screen w-full items-center justify-center overflow-clip bg-black/50 p-10 backdrop-blur-sm">
+    <div className="fixed left-0 top-0 z-[9999999] flex h-screen w-full items-center justify-center overflow-clip bg-black/50 backdrop-blur-sm">
+      <button
+        className="absolute right-5 top-5 flex size-8 items-center justify-center rounded-full bg-gray-200 text-2xl text-black"
+        onClick={closePreview}
+      >
+        <MdClose />
+      </button>
       <img
         src={url}
         alt=""
-        className="max-h-full w-full max-w-[1000px] object-cover object-center"
+        className="h-full max-w-full object-contain object-center"
         ref={imageRef}
       />
     </div>
