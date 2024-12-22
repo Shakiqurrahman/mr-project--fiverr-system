f53a9d27cd57d1cb9cd2caafd9142b6948698303
Developer Shakil <itsdevelopershakil@gmail.com>
2024-12-23 02:55:54 +0600

    extend delivery preview updated

--- src/components/order/chatbox-components/ExtendingDeliveryPreview.jsx
+++ src/components/order/chatbox-components/ExtendingDeliveryPreview.jsx
@@ -1,13 +1,60 @@
-import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+import React, { useState } from "react";
 import toast from "react-hot-toast";
 import { useSelector } from "react-redux";
 import { useUpdateAOrderMessageMutation } from "../../../Redux/api/orderApiSlice";
+import { STRIPE_PUBLIC_KEY, configApi } from "../../../libs/configApi";
+
+const stripePromise = loadStripe(STRIPE_PUBLIC_KEY);
 
 const ExtendingDeliveryPreview = ({ messageObj, value }) => {
+  const { user } = useSelector((state) => state?.user);
   const { projectDetails } = useSelector((state) => state.order);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Checking Admin
+  const isAdmin = ["ADMIN", "SUPER_ADMIN", "SUB_ADMIN"].includes(user?.role);
+
   const [updateAOrderMessage] = useUpdateAOrderMessageMutation();
   const handleAccept = (e) => {
     e.preventDefault();
+    console.log("accept");
+  };
+  const handleAcceptWithPayment = async (e) => {
+    e.preventDefault();
+    console.log("accept payment");
+    if (messageObj?.uniqueId) {
+      setIsLoading(true);
+      const { id, ...updateMessage } = messageObj;
+      const data = {
+        totalAmount: value?.amount,
+        paymentType: "ExtendDelivery",
+        updatedMessage: updateMessage,
+        duration: value?.days,
+        orderId: projectDetails?.id,
+        requestedByClient: false,
+        userId: user?.id,
+        projectNumber: projectDetails?.projectNumber,
+      };
+      try {
+        const response = await axios.post(
+          `${configApi.api}payment/extendad-delivery`,
+          {
+            data,
+          },
+        );
+
+        const sessionId = response?.data?.data?.id;
+        // Redirect to Stripe Checkout
+        const stripe = await stripePromise;
+        await stripe.redirectToCheckout({ sessionId });
+      } catch (error) {
+        toast.error("Something went wrong!");
+      }
+      setIsLoading(false);
+    }
   };
   const handleReject = async (e) => {
     e.preventDefault();
@@ -26,6 +73,7 @@ const ExtendingDeliveryPreview = ({ messageObj, value }) => {
       }
     }
   };
+  console.log(value);
   return (
     <div className="mt-5 border bg-lightskyblue">
       <div className="flex items-center border-b bg-[#CCE5FB] p-4 text-lg font-semibold">
@@ -53,13 +101,44 @@ const ExtendingDeliveryPreview = ({ messageObj, value }) => {
 
         {!value?.isAccepted &&
           !value?.isRejected &&
+          value?.isSubmittedByAdmin &&
+          user?.role === "USER" &&
+          projectDetails?.projectStatus !== "Completed" &&
+          projectDetails?.projectStatus !== "Canceled" && (
+            <div className="flex justify-center gap-8 border-t pt-6">
+              <button
+                type="submit"
+                onClick={
+                  value?.extendType === "requestByClient"
+                    ? handleAcceptWithPayment
+                    : handleAccept
+                }
+                disabled={isLoading}
+                className="w-[150px] bg-primary px-5 py-2 text-lg font-semibold text-white outline-none duration-300 hover:bg-primary/80 disabled:bg-primary/50"
+              >
+                Accept
+              </button>
+              <button
+                type="button"
+                onClick={handleReject}
+                className="w-[150px] bg-gray-500 px-5 py-2 text-lg font-semibold text-white outline-none duration-300 hover:bg-gray-500/80"
+              >
+                Decline
+              </button>
+            </div>
+          )}
+        {!value?.isAccepted &&
+          !value?.isRejected &&
+          !value?.isSubmittedByAdmin &&
+          isAdmin &&
           projectDetails?.projectStatus !== "Completed" &&
           projectDetails?.projectStatus !== "Canceled" && (
             <div className="flex justify-center gap-8 border-t pt-6">
               <button
                 type="submit"
                 onClick={handleAccept}
-                className="w-[150px] bg-primary px-5 py-2 text-lg font-semibold text-white outline-none duration-300 hover:bg-primary/80"
+                disabled={isLoading}
+                className="w-[150px] bg-primary px-5 py-2 text-lg font-semibold text-white outline-none duration-300 hover:bg-primary/80 disabled:bg-primary/50"
               >
                 Accept
               </button>
