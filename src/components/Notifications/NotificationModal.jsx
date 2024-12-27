6e2ceb7cdf6d3e48cc8c2e66eefd796719cc98fa
Developer Shakil <itsdevelopershakil@gmail.com>
2024-12-27 17:02:50 +0600

    notification modal responsive fixed

--- src/components/Notifications/NotificationModal.jsx
+++ src/components/Notifications/NotificationModal.jsx
@@ -3,12 +3,12 @@ import { IoClose } from "react-icons/io5";
 import { MdOutlineNotifications } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { useGetNotificationQuery } from "../../Redux/api/apiSlice";
+import { setOnlineUsers } from "../../Redux/features/userSlice";
 import useOutsideClick from "../../hooks/useOutsideClick";
 import { configApi } from "../../libs/configApi";
 import { connectSocket } from "../../libs/socketService";
 import { formatTimeAgo } from "../../libs/timeFormatter";
-import { useGetNotificationQuery } from "../../Redux/api/apiSlice";
-import { setOnlineUsers } from "../../Redux/features/userSlice";
 import GetNotificationTitle from "./GetNotificationTitle";
 
 const NotificationModal = ({ close }) => {
@@ -35,15 +35,15 @@ const NotificationModal = ({ close }) => {
   useOutsideClick(notificalModal, () => dispatch(close(false)));
   return (
     <div
-      className="static max-w-[400px] rounded-md bg-white shadow-lg sm:absolute sm:right-0 sm:top-10 md:w-[450px]"
+      className="static w-full max-w-[400px] rounded-md bg-white shadow-lg sm:absolute sm:top-10 md:right-0 md:w-[450px]"
       ref={notificalModal}
     >
       <div>
-        <h2 className="flex items-center justify-between gap-2 border-b p-4 text-base font-semibold text-black sm:justify-start">
+        <h2 className="flex items-center justify-between gap-2 border-b p-4 text-base font-semibold text-black md:justify-start">
           <MdOutlineNotifications className="text-2xl" /> Notifications (
           {unReadNotifications})
           <button
-            className="block sm:hidden"
+            className="block md:hidden"
             type="button"
             onClick={() => dispatch(close(false))}
           >
@@ -53,71 +53,73 @@ const NotificationModal = ({ close }) => {
 
         {/* List of unread notifications */}
         <div className="max-h-[400px] overflow-y-auto">
-          {notificationData?.map((notification) => {
-            const letterLogo = notification?.payload?.senderUserName
-              ?.trim()
-              ?.charAt(0)
-              ?.toUpperCase();
+          {notificationData
+            ?.map((notification) => {
+              const letterLogo = notification?.payload?.senderUserName
+                ?.trim()
+                ?.charAt(0)
+                ?.toUpperCase();
 
-            const notificationTitle = GetNotificationTitle({
-              type: notification?.payload?.type,
-              userName: notification?.payload?.senderUserName,
-              message: notification?.message,
-              rating: notification?.rating,
-              commentQuantity: notification?.commentQuantity,
-              hours: notification?.payload?.hours,
-              days: notification?.payload?.days,
-            });
-            return (
-              <div
-                key={notification?.id}
-                className="flex items-center justify-between gap-4 border-b p-4 last:border-b-0"
-              >
-                <div className="flex items-center gap-2">
-                  {/* avatar  */}
-                  <div className="relative mx-auto flex size-10 items-center justify-center rounded-full border border-gray-300 bg-[#ffefef]/30 sm:size-14">
-                    {notification?.payload?.avatar ? (
+              const notificationTitle = GetNotificationTitle({
+                type: notification?.payload?.type,
+                userName: notification?.payload?.senderUserName,
+                message: notification?.message,
+                rating: notification?.rating,
+                commentQuantity: notification?.commentQuantity,
+                hours: notification?.payload?.hours,
+                days: notification?.payload?.days,
+              });
+              return (
+                <div
+                  key={notification?.id}
+                  className="flex items-center justify-between gap-4 border-b p-4 last:border-b-0"
+                >
+                  <div className="flex items-center gap-2">
+                    {/* avatar  */}
+                    <div className="relative mx-auto flex size-10 items-center justify-center rounded-full border border-gray-300 bg-[#ffefef]/30 sm:size-14">
+                      {notification?.payload?.avatar ? (
+                        <img
+                          className="h-full w-full rounded-full object-cover"
+                          src={notification?.payload?.avatar}
+                          alt="Sender Logo"
+                        />
+                      ) : (
+                        <div className="text-3xl font-bold text-[#7c7c7c]/50 sm:text-4xl">
+                          {letterLogo}
+                        </div>
+                      )}
+                      <span
+                        className={`absolute bottom-0 right-1 size-3 rounded-full border border-white ${isUserOnline(notification?.payload?.userId) ? "bg-primary" : "bg-gray-400"}`}
+                      ></span>
+                    </div>
+
+                    {/* msg  */}
+                    <div className="flex flex-1 flex-col gap-2">
+                      <div>{notificationTitle}</div>
+                      {notification?.payload?.createdAt && (
+                        <p className="text-sm text-gray-600">
+                          {formatTimeAgo(notification?.payload?.createdAt)}
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                  {notification?.payload?.thumbnailUrl && (
+                    <Link
+                      className="w-24 flex-grow-0"
+                      to={`/order/${notification?.payload?.projectNumber}`}
+                      onClick={() => dispatch(close(false))}
+                    >
                       <img
-                        className="h-full w-full rounded-full object-cover"
-                        src={notification?.payload?.avatar}
-                        alt="Sender Logo"
+                        className="w-full rounded-lg object-cover"
+                        src={notification?.payload?.thumbnailUrl}
+                        alt="ordered image"
                       />
-                    ) : (
-                      <div className="text-3xl font-bold text-[#7c7c7c]/50 sm:text-4xl">
-                        {letterLogo}
-                      </div>
-                    )}
-                    <span
-                      className={`absolute bottom-0 right-1 size-3 rounded-full border border-white ${isUserOnline(notification?.payload?.userId) ? "bg-primary" : "bg-gray-400"}`}
-                    ></span>
-                  </div>
-
-                  {/* msg  */}
-                  <div className="flex flex-1 flex-col gap-2">
-                    <div>{notificationTitle}</div>
-                    {notification?.payload?.createdAt && (
-                      <p className="text-sm text-gray-600">
-                        {formatTimeAgo(notification?.payload?.createdAt)}
-                      </p>
-                    )}
-                  </div>
+                    </Link>
+                  )}
                 </div>
-                {notification?.payload?.thumbnailUrl && (
-                  <Link
-                    className="w-24 flex-grow-0"
-                    to={`/order/${notification?.payload?.projectNumber}`}
-                    onClick={() => dispatch(close(false))}
-                  >
-                    <img
-                      className="w-full rounded-lg object-cover"
-                      src={notification?.payload?.thumbnailUrl}
-                      alt="ordered image"
-                    />
-                  </Link>
-                )}
-              </div>
-            );
-          }).reverse()}
+              );
+            })
+            .reverse()}
           {notificationData?.length === 0 && (
             <p className="py-3 text-center text-black">
               No notifications found.
