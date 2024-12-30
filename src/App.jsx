92046d036beb53f740adf5a5c60c64f4f2e8a607
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-12-31 03:50:32 +0600

    notification wrapper added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
 import axios from "axios";
-import Cookies from "js-cookie";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import Layout from "./AllRoutes/Layout";
 import { router } from "./AllRoutes/Routes";
-import store from "./Redux/store";
-import NotificationPopper from "./components/Notifications/NotificationPopper";
 import AuthWrapper from "./libs/AuthWrapper";
 import { configApi } from "./libs/configApi";
-import { connectSocket } from "./libs/socketService";
+import NotificationWrapper from "./libs/NotificationWrapper";
+import store from "./Redux/store";
 function App() {
-  const token = Cookies.get("authToken");
-  const socket = connectSocket(`${configApi.socket}`, token);
-
-  const [notification, setNotification] = useState(null);
-  const [showNotification, setShowNotification] = useState(false);
-
   useEffect(() => {
     const hasVisited = JSON.parse(localStorage.getItem("hasVisited"));
 
@@ -33,28 +25,15 @@ function App() {
     }
   }, []);
 
-  // Listen to new notifications from the server
-  useEffect(() => {
-    socket?.on("get:notification", (notification) => {
-      console.log("notification", notification);
-      setNotification(notification);
-      setShowNotification(true);
-    });
-  }, [socket]);
-
   return (
     <Provider store={store}>
-      <AuthWrapper>
-        <RouterProvider router={router}>
-          <Layout />
-        </RouterProvider>
-      </AuthWrapper>
-      {showNotification && (
-        <NotificationPopper
-          onClose={() => setShowNotification(false)}
-          notification={notification}
-        />
-      )}
+      <NotificationWrapper>
+        <AuthWrapper>
+          <RouterProvider router={router}>
+            <Layout />
+          </RouterProvider>
+        </AuthWrapper>
+      </NotificationWrapper>
     </Provider>
   );
 }
