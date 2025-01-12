367edbc7dc9548e59390a175dac01578fa2031d9
Developer Shakil <itsdevelopershakil@gmail.com>
2025-01-13 01:19:31 +0600

    navbar design and industry keywords limited to 30 done

--- src/components/partials/DropDownMenu.jsx
+++ src/components/partials/DropDownMenu.jsx
@@ -17,32 +17,63 @@ const DropDownMenu = ({ className, isDesign, isIndustry }) => {
     isLoading: isLoadingIndustry,
     error: errorIndustry,
   } = useFetchAllIndustryKeywordsQuery();
+
+  const slicedDesignKeywordsData =
+    designKeywordsData?.length > 30
+      ? designKeywordsData?.slice(0, 30)
+      : designKeywordsData;
+  const slicedIndustryKeywordsData =
+    industryKeywordsData?.length > 30
+      ? industryKeywordsData?.slice(0, 30)
+      : industryKeywordsData;
   return (
     <div
       className={`${className} absolute left-0 top-full w-full border-t bg-primary py-6`}
     >
       <div className="flex flex-wrap justify-center gap-4">
-        {isDesign &&
-          designKeywordsData?.map((designKeyword, idx) => (
-            <button
-              key={idx}
-              onClick={() => navigate("/designs", { state: designKeyword })}
-              className="rounded-[30px] bg-white/20 px-2 py-1 text-sm font-medium text-white duration-300 hover:bg-white hover:text-primary sm:px-4 sm:py-2 sm:text-base"
-            >
-              {designKeyword}
-            </button>
-          ))}
-        {isIndustry &&
-          industryKeywordsData?.map((industryKeyword, idx) => (
-            <Link
-              key={idx}
-              to={"/industries"}
-              state={industryKeyword}
-              className="rounded-[30px] bg-white/20 px-2 py-1 text-sm font-medium text-white duration-300 hover:bg-white hover:text-primary sm:px-4 sm:py-2 sm:text-base"
-            >
-              {industryKeyword}
-            </Link>
-          ))}
+        {isDesign && (
+          <>
+            {slicedDesignKeywordsData?.map((designKeyword, idx) => (
+              <button
+                key={idx}
+                onClick={() => navigate("/designs", { state: designKeyword })}
+                className="rounded-[30px] bg-white/20 px-2 py-1 text-sm font-medium text-white duration-300 hover:bg-white hover:text-primary sm:px-4 sm:py-2 sm:text-base"
+              >
+                {designKeyword}
+              </button>
+            ))}
+            {designKeywordsData?.length > 30 && (
+              <button
+                onClick={() => navigate("/designs")}
+                className="rounded-[30px] bg-white/20 px-2 py-1 text-sm font-medium text-white duration-300 hover:bg-white hover:text-primary sm:px-4 sm:py-2 sm:text-base"
+              >
+                See More
+              </button>
+            )}
+          </>
+        )}
+        {isIndustry && (
+          <>
+            {slicedIndustryKeywordsData?.map((industryKeyword, idx) => (
+              <Link
+                key={idx}
+                to={"/industries"}
+                state={industryKeyword}
+                className="rounded-[30px] bg-white/20 px-2 py-1 text-sm font-medium text-white duration-300 hover:bg-white hover:text-primary sm:px-4 sm:py-2 sm:text-base"
+              >
+                {industryKeyword}
+              </Link>
+            ))}
+            {industryKeywordsData?.length > 30 && (
+              <Link
+                to={"/industries"}
+                className="rounded-[30px] bg-white/20 px-2 py-1 text-sm font-medium text-white duration-300 hover:bg-white hover:text-primary sm:px-4 sm:py-2 sm:text-base"
+              >
+                See More
+              </Link>
+            )}
+          </>
+        )}
         {isLoadingIndustry && isLoadingDesign && "Loading..."}
         {errorDesign && errorIndustry && "Something went wrong!"}
       </div>
