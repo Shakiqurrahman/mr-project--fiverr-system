0ef835f70b38c0bb73bdb9e0622cb613d9864731
Shakiqurrahman <rahmanshakiqur@gmail.com>
2025-01-19 00:50:50 +0600

    active projects late color added

--- src/components/customer-profile/ActiveProjects.jsx
+++ src/components/customer-profile/ActiveProjects.jsx
@@ -11,67 +11,9 @@ import React from "react";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { getStatusText } from "./StatusText";
+import { getTimeStatus } from "../../libs/getTimeStatus";
 
 const ActiveProjects = ({ activeProjects, isActiveProjectsLoading }) => {
-  // Function to get time status
-  const getTimeStatus = (deadline) => {
-    const now = new Date();
-    const eventDate = parseISO(deadline); // Convert string to date
-
-    if (isPast(eventDate)) {
-      // time is late
-      const yearsLate = differenceInYears(now, eventDate);
-      const monthsLate = differenceInMonths(now, eventDate) % 12;
-      const daysLate = differenceInDays(now, eventDate) % 30;
-      const hoursLate = differenceInHours(now, eventDate) % 24;
-      const minutesLate = differenceInMinutes(now, eventDate) % 60;
-
-      let overdueText = "";
-
-      if (yearsLate >= 1) {
-        overdueText = `${yearsLate} year${yearsLate > 1 ? "s" : ""} late`;
-      } else if (monthsLate >= 1) {
-        overdueText = `${monthsLate} month${monthsLate > 1 ? "s" : ""} late`;
-      } else if (daysLate >= 1) {
-        overdueText = `${daysLate} day${daysLate > 1 ? "s" : ""}`;
-      } else if (hoursLate >= 1) {
-        overdueText = `${hoursLate}h ${minutesLate}min late`;
-      } else {
-        overdueText = `${minutesLate}min late`;
-      }
-
-      return {
-        time: overdueText,
-        color: "black", // Default color for overdue events
-      };
-    } else {
-      // time is remaining
-      const timeRemaining = eventDate - now;
-
-      const totalHours = Math.floor(timeRemaining / (1000 * 60 * 60)); // Total hours remaining
-      const days = Math.floor(totalHours / 24); // Calculate remaining days
-      const hours = totalHours % 24; // Remaining hours
-      const minutes = Math.floor(
-        (timeRemaining % (1000 * 60 * 60)) / (1000 * 60),
-      ); // Remaining minutes
-
-      let displayTime;
-      if (days > 0) {
-        displayTime = `${days}d - ${hours}h`;
-      } else {
-        displayTime = `${hours}h - ${minutes} min`;
-      }
-
-      // Set color based on remaining time
-      const color = totalHours < 12 ? "red" : "black"; // Red if less than 12 hours
-
-      return {
-        time: displayTime,
-        color: color,
-      };
-    }
-  };
-
   return (
     <div className="mb-16 mt-8 grid gap-4 min-[850px]:grid-cols-2">
       {isActiveProjectsLoading ? (
